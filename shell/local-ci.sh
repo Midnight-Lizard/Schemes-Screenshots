a377528e71f9edff5e11e7fd3389110df8ef9866
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-09-17 17:43:45 +0300

    minikube debug

--- shell/local-ci.sh
+++ shell/local-ci.sh
@@ -8,7 +8,7 @@ eval $(docker-machine env default --shell bash)
 docker build -t $IMAGE \
     --build-arg DOTNET_CONFIG=Build \
     --build-arg INSTALL_CLRDBG="apt-get update \
-        && apt-get install -y --no-install-recommends unzip \
+        && apt-get install -y --no-install-recommends unzip curl \
         && rm -rf /var/lib/apt/lists/* \
         && curl -sSL https://aka.ms/getvsdbgsh | bash /dev/stdin -v latest -l /vsdbg" \
     ../
@@ -16,13 +16,21 @@ kubectl config use-context minikube
 docker push $IMAGE
 
 # getting value from local env var
-cloudinaryUrl=$($CLOUDINARY_URL | base64 -w 0);
+cloudinaryUrl=$(echo -n "$CLOUDINARY_URL" | base64 -w 0);
 
     # --set env.SCREENSHOT_SIZES=1280x800x200~640x400x200 \
     # --set env.SCREENSHOT_OUT_DIR=./wwwroot \
+    # --set env.SCREENSHOT_URLS=https://www.google.com/search?hl\=en\&q\={colorSchemeName} \
 
 helm upgrade --install --set image=$IMAGE \
     --set env.ASPNETCORE_ENVIRONMENT=Development \
-    --set env.SCREENSHOT_URLS=https://www.google.com/search?hl\=en\&q\={colorSchemeName} \
     --set secrets.cloudinaryUrl=$cloudinaryUrl \
+    --set livenessProbe.initialDelaySeconds=30 \
+    --set livenessProbe.periodSeconds=90 \
+    --set livenessProbe.timeoutSeconds=60 \
+    --set readinessProbe.initialDelaySeconds=20 \
+    --set readinessProbe.periodSeconds=30 \
+    --set readinessProbe.timeoutSeconds=30 \
+    --set screenshotConfig.cdnIdTemplate=tmp/{id}/{title}/{size} \
+    --set screenshotConfig.cdnPrefixTemplate=tmp/{id}/ \
     $PROJ ../kube/$PROJ
