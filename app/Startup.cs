31ba75d6c5b780f14ef97abd9518db524a557339
Pavel Agarkov <pavel.agarkov@gmail.com>
2019-07-23 23:24:03 +0300

    mvc CompatibilityVersion upd

--- app/Startup.cs
+++ app/Startup.cs
@@ -49,7 +49,7 @@ namespace MidnightLizard.Schemes.Screenshots
             services.AddSingleton<IScreenshotUploader, ScreenshotUploader>();
             services.AddSingleton<IProgressiveImageConverter, ProgressiveImageConverter>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
